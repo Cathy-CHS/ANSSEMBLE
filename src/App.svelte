6f437a33d144944996891471437b3005194c0fe1
Haeseul <jjchsjj@gmail.com>
2023-06-01 03:03:41 +0900

    Enable sound play by pressing button

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,39 @@
 <script>
 	export let name;
-	import p5 from 'p5-svelte';
+	// import P5 from 'p5-svelte';
+	// import * as P5play from 'p5play';
+	import Button from "./Button.svelte";
 	import * as Tone from 'tone';
-	const s = new Tone.Synth().toDestination();
-	s.triggerAttackRelease("C4", "8n");
+	// const synth = new Tone.Synth().toDestination();
+	const piano = new Tone.Sampler({
+		urls: {
+			"C4": "./assets/piano/C4.mp3",
+			"Db4": "./assets/piano/Db4.mp3",
+			"D4": "./assets/piano/D4.mp3",
+			"Eb4": "./assets/piano/Eb4.mp3",
+			"E4": "./assets/piano/E4.mp3",
+			"F4": "./assets/piano/F4.mp3",
+			"Gb4": "./assets/piano/Gb4.mp3",
+			"G4": "./assets/piano/G4.mp3",
+			"Ab4": "./assets/piano/Ab4.mp3",
+			"A4": "./assets/piano/A4.mp3",
+			"Bb4": "./assets/piano/Bb4.mp3",
+			"B4": "./assets/piano/B4.mp3",
+		},
+		baseUrl: "https://github.com/fuhton/piano-mp3/tree/master/piano-mp3",
+		release: 1,
+	}).toDestination();
+	Tone.Transport.bpm.value = 60; // probably can define seperately as constant
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Button class="sound" on:click={()=>{
+	// synth.triggerAttackRelease("C4", "4n");
+	// synth.triggerRelease(4n+1);
+	Tone.loaded().then(() => {
+		piano.triggerAttackRelease(["Eb4", "G4", "Bb4"], "4n");
+	});
+}}>
+	C4
+</Button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
-
-<!-- <p5 {sketch} /> -->
+<!-- <P5 {sketch} /> -->
---
+++ src/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+	
+	let buttonProps = {
+	class:[$$restProps.class]
+	}
+</script>
+	<button on:click
+            on:mouse
+            on:focus
+			on:mousedown
+			on:mouseover
+			on:mouseenter
+			on:mouseleave
+		{...buttonProps}>
+			<slot/>
+	</button>
+
+<style>
+	.sound{
+		color:green;
+	}
+</style>
