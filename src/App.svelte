82dfbc7f88a76514ad819d5dbbe7dfd167249b84
Haeseul <jjchsjj@gmail.com>
2023-06-01 03:13:15 +0900

    Fix the button function with Sampler

--- src/App.svelte
+++ src/App.svelte
@@ -7,20 +7,25 @@
 	// const synth = new Tone.Synth().toDestination();
 	const piano = new Tone.Sampler({
 		urls: {
-			"C4": "./assets/piano/C4.mp3",
-			"Db4": "./assets/piano/Db4.mp3",
-			"D4": "./assets/piano/D4.mp3",
-			"Eb4": "./assets/piano/Eb4.mp3",
-			"E4": "./assets/piano/E4.mp3",
-			"F4": "./assets/piano/F4.mp3",
-			"Gb4": "./assets/piano/Gb4.mp3",
-			"G4": "./assets/piano/G4.mp3",
-			"Ab4": "./assets/piano/Ab4.mp3",
-			"A4": "./assets/piano/A4.mp3",
-			"Bb4": "./assets/piano/Bb4.mp3",
-			"B4": "./assets/piano/B4.mp3",
+			// "C4": "C4.mp3",
+			// "Db4": "Db4.mp3",
+			// "D4": "D4.mp3",
+			// "Eb4": "Eb4.mp3",
+			// "E4": "E4.mp3",
+			// "F4": "F4.mp3",
+			// "Gb4": "Gb4.mp3",
+			// "G4": "G4.mp3",
+			// "Ab4": "Ab4.mp3",
+			// "A4": "A4.mp3",
+			// "Bb4": "Bb4.mp3",
+			// "B4": "B4.mp3",
+			"C4": "C4.mp3",
+			"D#4": "Ds4.mp3",
+			"F#4": "Fs4.mp3",
+			"A4": "A4.mp3",
 		},
-		baseUrl: "https://github.com/fuhton/piano-mp3/tree/master/piano-mp3",
+		// baseUrl: "https://github.com/Cathy-CHS/ID311_FinalProject/tree/Haeseul/assets/piano/",
+		baseUrl: "https://tonejs.github.io/audio/salamander/",
 		release: 1,
 	}).toDestination();
 	Tone.Transport.bpm.value = 60; // probably can define seperately as constant
@@ -29,6 +34,7 @@
 <Button class="sound" on:click={()=>{
 	// synth.triggerAttackRelease("C4", "4n");
 	// synth.triggerRelease(4n+1);
+	async () => {await Tone.start();}
 	Tone.loaded().then(() => {
 		piano.triggerAttackRelease(["Eb4", "G4", "Bb4"], "4n");
 	});
