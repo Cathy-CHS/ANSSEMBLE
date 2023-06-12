883531ee07e5cad32e673cd63562cfb63a513f5b
heejin kim <h_jjin@kaist.ac.kr>
2023-06-12 14:36:55 +0900

    main screen

--- src/mainscreen.svelte
+++ src/mainscreen.svelte
@@ -8,9 +8,9 @@
 
 const sketch = (p5) => {
     let input;
-    let input2;
     let button;
-    let button2;
+    let namelist = ['KHJ', 'LNH', 'CHS']
+
 
 
     p5.setup = () => {
@@ -23,44 +23,76 @@ const sketch = (p5) => {
     button.position(p5.width/2+p5.windowWidth/2000*200,p5.height/2);
     button.mousePressed(enter);
     button.style('font-size', 40*p5.width/2000+'px');
+    button.style('background-color', p5.color(255, 255, 255, 255));
     };
 
 
     p5.draw = () => {
      let width_ratio = p5.width/2000;
-     let height_ratio = p5.height/1200;
      p5.background(0);
      p5.fill(255);
      p5.textSize(width_ratio*150);
-     p5.text('ID311',p5.width/2-width_ratio*200,p5.height/2-width_ratio*100);
-     p5.noStroke();
+     p5.text('ANSSEMBLE',p5.width/2-width_ratio*400,p5.height/2-width_ratio*100);
+
+     p5.stroke()
+
      p5.textSize(width_ratio*50);
+     p5.fill(153,228,211,200);
+     p5.ellipse(width_ratio*600,width_ratio*400,width_ratio*150,width_ratio*150);
+
+     p5.fill(147,199,241,200);
+     p5.ellipse(width_ratio*1200,width_ratio*500,width_ratio*80,width_ratio*80);
+
+     p5.fill(242,147,199,200);
+     p5.ellipse(width_ratio*1390,width_ratio*350,width_ratio*50,width_ratio*50);
+
+     p5.fill(250,222,150,200);
+     p5.ellipse(width_ratio*1430,width_ratio*380,width_ratio*30,width_ratio*30);
+
+     p5.fill(255);
+     p5.textSize(width_ratio*150);
+     p5.text('ANSSEMBLE',p5.width/2-width_ratio*400,p5.height/2-width_ratio*100);
+
+     p5.fill(250,222,150,100);
+     p5.ellipse(width_ratio*100,width_ratio*100,width_ratio*400,width_ratio*400);
+
+     p5.fill(242,147,199,200);
+     p5.ellipse(width_ratio*1700,width_ratio*1000,width_ratio*200,width_ratio*200);
+
+     p5.fill(153,228,211,200);
+     p5.ellipse(width_ratio*1800,width_ratio*900,width_ratio*100,width_ratio*100);
+
+     p5.fill(255,255,255,200);
+     p5.ellipse(width_ratio*300,width_ratio*1000,width_ratio*50,width_ratio*50);
     };
 
     function enter(){
-        p5.textSize(30);
+        let width_ratio = p5.width/2000;
+        p5.textSize(50);
         var name = input.value();
         console.log(name)
 // 사용자 닉네임 입력
 
-        input2 = p5.createInput();
-        input2.position(p5.width/2-p5.windowWidth/2000*300, p5.height/2);
-        input2.style('font-size', 40*p5.width/2000+'px');
+        if (name == ''){
+            //입력 안함
+            p5.fill(255);
+            console.log('Please type your name...');
+            p5.text('Please type your name again...',p5.width/2-width_ratio*150,p5.height/2+width_ratio*100);
+        }
 
+        else if (!namelist.includes(name)){
+            //리스트에 닉네임 없음
+            p5.fill(255);
+            console.log('Please check your name again...');
+            p5.text('Please check your name again...',p5.width/2-width_ratio*400,p5.height/2+width_ratio*100);
+        }
 
-        button2 = p5.createButton("ENTER");
-        button2.position(p5.width/2+p5.windowWidth/2000*200,p5.height/2);
-        button2.mousePressed(enter2);
-        button2.style('font-size', 40*p5.width/2000+'px');
+        else{
+            p5.fill(255);
+            console.log('hello!');
+            p5.text('HELLO',p5.width/2-width_ratio*400,p5.height/2+width_ratio*100);
+        }
     }
-
-    function enter2(){
-    //// 노래 검색
-        p5.textSize(30);
-        var name = input2.value();
-        console.log(name)
-    }
-   
 };
 
 </script>
