a0fb611dacac1d75b7c4dcd70037660d76b01ff0
heejin kim <h_jjin@kaist.ac.kr>
2023-06-14 21:34:29 +0900

    bubble moves

--- src/mainscreen.svelte
+++ src/mainscreen.svelte
@@ -9,9 +9,13 @@
 const sketch = (p5) => {
     let input;
     let button;
-    let namelist = ['KHJ', 'LNH', 'CHS']
     
     let ellipse1 = [p5.windowWidth/2000*300,p5.windowWidth/2000*300,p5.windowWidth/2000*300,p5.random(5),p5.random(5)];
+    let ellipse2 = [p5.windowWidth/2000*800,p5.windowWidth/2000*800,p5.windowWidth/2000*250,p5.random(5),p5.random(5)];
+    let ellipse3 = [p5.windowWidth/2000*1500,p5.windowWidth/2000*850,p5.windowWidth/2000*270,p5.random(5),p5.random(5)];
+    let ellipse4 = [p5.windowWidth/2000*300,p5.windowWidth/2000*1000,p5.windowWidth/2000*200,p5.random(5),p5.random(5)];
+    let ellipse5 = [p5.windowWidth/2000*180,p5.windowWidth/2000*500,p5.windowWidth/2000*240,p5.random(5),p5.random(5)];
+    let ellipse6 = [p5.windowWidth/2000*1600,p5.windowWidth/2000*200,p5.windowWidth/2000*200,p5.random(5),p5.random(5)];
 
 
 
@@ -26,6 +30,7 @@ const sketch = (p5) => {
     button.position(p5.width/2+p5.windowWidth/2000*200,p5.height/2);
     button.mousePressed(enter);
     button.style('font-size', 40*p5.width/2000+'px');
+    button.style('back-ground', p5.color(255,255,255,255));
     };
 
 
@@ -38,11 +43,15 @@ const sketch = (p5) => {
      p5.noStroke(255);
 
 
-     makeellipse(ellipse1)
+   
 
 
 
      p5.textSize(width_ratio*50);
+
+
+
+
 
 
 
@@ -58,29 +67,38 @@ const sketch = (p5) => {
      p5.fill(250,222,150,200);
      p5.ellipse(width_ratio*1430,width_ratio*380,width_ratio*30);
 
+
+
+
+
+
+
+
+     p5.fill(250,222,150,150);
+     makeellipse(ellipse1);
+
+     p5.fill(242,147,199,150);
+     makeellipse(ellipse2);
+
+     p5.fill(153,228,211,150);
+     makeellipse(ellipse3);
+
+     p5.fill(255,255,255,150);
+     makeellipse(ellipse4);
+
+
+     p5.fill(110,44,255,150);
+     makeellipse(ellipse5);
+
+     p5.fill(160,239,146,150);
+     makeellipse(ellipse6);
+
+
      p5.fill(255);
      p5.textSize(width_ratio*150);
      p5.text('ANSSEMBLE',p5.width/2-width_ratio*400,p5.height/2-width_ratio*100);
 
 
-
-
-
-
-     p5.fill(250,222,150,100);
-     p5.ellipse(width_ratio*100,width_ratio*100,width_ratio*700);
-
-     p5.fill(242,147,199,200);
-     p5.ellipse(width_ratio*1700,width_ratio*1000,width_ratio*500);
-
-     p5.fill(153,228,211,200);
-     p5.ellipse(width_ratio*1800,width_ratio*900,width_ratio*400);
-
-     p5.fill(255,255,255,200);
-     p5.ellipse(width_ratio*300,width_ratio*1000,width_ratio*300);
-
-     p5.fill(255,255,255,200);
-     p5.ellipse(width_ratio*300,width_ratio*1000,width_ratio*300);
     };
 
     function enter(){
@@ -95,13 +113,6 @@ const sketch = (p5) => {
             p5.fill(255);
             console.log('Please type your name...');
             p5.text('Please type your name again...',p5.width/2-width_ratio*150,p5.height/2+width_ratio*100);
-        }
-
-        else if (!namelist.includes(name)){
-            //리스트에 닉네임 없음
-            p5.fill(255);
-            console.log('Please check your name again...');
-            p5.text('Please check your name again...',p5.width/2-width_ratio*400,p5.height/2+width_ratio*100);
         }
 
         else{
@@ -119,8 +130,6 @@ const sketch = (p5) => {
     
         let xspeed = ellipse[3];
         let yspeed = ellipse[4];
-
-        console.log(ellipse[0],ellipse[1],ellipse[2],ellipse[3],ellipse[4]);
 
         p5.ellipse(x, y, r*2, r*2)
 
